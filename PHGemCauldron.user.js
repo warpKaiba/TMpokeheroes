9666a2d5bde5e790a7815ea42a091eb0c3f1c4c3
kaiba <27407917+warpKaiba@users.noreply.github.com>
2020-04-01 15:57:30 -0500

    Update PHGemCauldron.user.js

--- PHGemCauldron.user.js
+++ PHGemCauldron.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PH - Gem Cauldron
 // @namespace    https://github.com/warpKaiba
-// @version      0.2
+// @version      0.1
 // @description  try to take over the world!
 // @author       You
 // @match        http*://pokeheroes.com/gem_cauldron*
